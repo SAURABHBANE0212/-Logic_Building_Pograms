0b36cff702922b25f79f2ed2f2e0223d008ed2b5
SAURABH NATHAJI BANE <132773188+SAURABHBANE0212@users.noreply.github.com>
2023-07-22 21:09:55 +0530

    Add files via upload

---
+++ CPP Program/1 Basic C++ Problem/program203.cpp
@@ -0,0 +1,11 @@
+//print Jay ganesh on screen
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh..."<<"\n";
+
+    return 0;
+}
---
+++ CPP Program/2 Problems on Bitwise Operator/program247.cpp
@@ -0,0 +1,42 @@
+// Accept Number fropm user and toggle the bit and return Modified Number
+
+#include<iostream>
+using namespace std;
+
+typedef unsigned int UINT;
+
+UINT ToggleBit(UINT iNo)
+{
+    UINT iMask = 0X00000040;
+    UINT iResult = 0;
+
+    iResult = iNo ^ iMask;
+
+    return iResult;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    cout<<"Enter number : "<<"\n";
+    cin>>iValue;
+
+    iRet = ToggleBit(iValue);
+    cout<<"Result is : "<<iRet<<"\n";
+
+    return 0;
+}
+
+/*
+
+    iPos = 7
+
+    iNo        0   0   1   1   0   1   0   0 
+               0   1   0   0   0   0   0   0        ^
+-----------------------------------------------------
+               0   0   1   1   0   1   0   0
+*/
+
+
